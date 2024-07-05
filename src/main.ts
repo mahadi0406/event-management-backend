46bbfe1c0f7cb21ea1fbf129134fe2623ee19086
sabbir8 <sabbir@cloudly.io>
2024-07-05 12:05:16 +0600

    added signup & store user database

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,9 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Event Management API')
+    .setDescription('The Event Management API description')
     .setVersion('1.0')
-    .addTag('cats')
-    .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document, {
     jsonDocumentUrl: 'event/json',
