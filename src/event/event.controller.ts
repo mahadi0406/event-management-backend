97f16add294498e1d67402839ac046fc0dfb2373
sabbir8 <sabbir@cloudly.io>
2024-07-08 11:46:34 +0600

    added event entity

--- src/event/event.controller.ts
+++ src/event/event.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { EventService } from './event.service';
+import { EventDto } from './event.dto';
 
 @Controller('event')
-export class EventController {}
+export class EventController {
+  constructor(private readonly eventService: EventService) {}
+
+  @HttpCode(HttpStatus.CREATED)
+  @Post('')
+  async create(@Body() eventDto: EventDto) {
+    const createdEvent = await this.eventService.create(eventDto);
+    return {
+      statusCode: HttpStatus.CREATED,
+      message: 'Event created successfully',
+      data: createdEvent,
+    };
+  }
+
+  @Put(':id')
+  @HttpCode(HttpStatus.OK)
+  async update(@Param('id') id: string, @Body() eventDto: EventDto) {
+    const updatedEvent = await this.eventService.update(+id, eventDto);
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Event updated successfully',
+      data: updatedEvent,
+    };
+  }
+}
--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { EventDto } from './event.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Event } from './event.entity';
 
 @Injectable()
-export class EventService {}
+export class EventService {
+  constructor(
+    @InjectRepository(Event)
+    private eventRepository: Repository<Event>,
+  ) {}
+
+  async findById(id: number) {
+    return this.eventRepository.findOne({ where: { id } });
+  }
+
+  async create(eventDto: EventDto) {
+    const newEvent = this.eventRepository.create(eventDto);
+    return await this.eventRepository.save(newEvent);
+  }
+
+  async update(id: number, eventDto: EventDto) {
+    const event = await this.findById(id);
+    if (!event) {
+      throw new NotFoundException(`Event with ID ${id} not found`);
+    }
+    await this.eventRepository.update(id, eventDto);
+    return this.findById(id);
+  }
+}
